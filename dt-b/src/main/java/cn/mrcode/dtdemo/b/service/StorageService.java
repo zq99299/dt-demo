721f4dfa1de3849173c1a3088835b8a82cc47be1
zq99299 <99299684@qq.com>
2025-06-01 14:11:49 +0800

    fix

--- dt-b/src/main/java/cn/mrcode/dtdemo/b/service/StorageService.java
+++ dt-b/src/main/java/cn/mrcode/dtdemo/b/service/StorageService.java
@@ -1,9 +1,5 @@
 package cn.mrcode.dtdemo.b.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-// StorageService.java
 public interface StorageService {
     void decrease(String productId, Integer count);
 }
